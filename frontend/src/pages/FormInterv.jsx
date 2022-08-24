3cdf9b47b9ab1d4a1da4b6d1d90eced097b32e45
Crab-Prog <ophelie.coudert@gmail.com>
2022-08-24 14:49:08 +0200

    form association contact done

--- frontend/src/pages/FormInterv.jsx
+++ frontend/src/pages/FormInterv.jsx
@@ -84,191 +84,195 @@ function FormInterv() {
       <div className="back">
         <form onSubmit={handleSubmit} method="post">
           <div className="register_form">
-            <h1>{`Demande d'inscription pour les intervenants`}</h1>
-            <div className="box_form">
-              <div className="form-group">
-                <input
-                  type="text"
-                  id="interv_nom"
-                  name="nom"
-                  onChange={handleChange}
-                  required
-                  className="form-control"
-                />
-                <label
-                  htmlFor="interv_nom"
-                  className="form-control-placeholder"
-                >
-                  Nom
-                </label>
-              </div>
+            <h1>Demande d'inscription pour les intervenants</h1>
 
-              <div className="form-group">
-                <input
-                  type="text"
-                  id="interv_prenom"
-                  name="prenom"
-                  onChange={handleChange}
-                  required
-                  className="form-control"
-                />
-                <label
-                  htmlFor="interv_prenom"
-                  className="form-control-placeholder"
-                >
-                  Prénom{" "}
-                </label>
-              </div>
-            </div>
-
-            <div className="box_form">
-              <div className="form-group">
-                <input
-                  type="email"
-                  id="interv_email"
-                  name="email"
-                  onChange={handleChange}
-                  required
-                  className="form-control"
-                />
-                <label
-                  htmlFor="interv_email"
-                  className="form-control-placeholder"
-                >
-                  Email{" "}
-                </label>
-              </div>
-
-              <div className="form-group">
-                <input
-                  type="text"
-                  id="interv_tel"
-                  name="telephone"
-                  onChange={handleChange}
-                  required
-                  className="form-control"
-                />
-                <label
-                  htmlFor="interv_tel"
-                  className="form-control-placeholder"
-                >
-                  Téléphone
-                </label>
-              </div>
-            </div>
-
-            <div className="box_form">
-              <div className="form-group">
-                <input
-                  type="password"
-                  name="password"
-                  id="interv_mdp"
-                  required
-                  onChange={handleChange}
-                  autoComplete="off"
-                  className="form-control"
-                />
-                <label
-                  htmlFor="interv_mdp"
-                  className="form-control-placeholder"
-                >
-                  Choisir un mot de passe
-                </label>
-              </div>
-
-              <div className="form-group">
-                <input
-                  type="password"
-                  name="passCheck"
-                  id="interv_mdp2"
-                  required
-                  onChange={handleChange}
-                  autoComplete="off"
-                  className="form-control"
-                />
-                <label
-                  htmlFor="interv_mdp"
-                  className="form-control-placeholder"
-                >
-                  Retapez votre mot de passe
-                </label>
-              </div>
-            </div>
-
-            <div className="form_upload">
-              <h2>
-                Merci de nous transmettre via ce formulaire les pièces
-                justificatives suivantes :
-              </h2>
-              <div className="form-inter-box-files">
-                <label htmlFor="upload_statut_ae" className="btn">
-                  <p>{`Une copie de votre statut d'autoentrepreneur`}</p>
-                  <div>
-                    <FaCloudUploadAlt className="upload_icon" />
-                  </div>
-                  {isDocSend("AutoE")}
+            <div className="form-inter-container">
+              <div className="box_form">
+                <div className="form-group">
                   <input
-                    id="upload_statut_ae"
-                    type="file"
-                    name="AutoE"
-                    onChange={(e) => {
-                      handleChange(e, "file");
-                    }}
-                    className="inputfile"
+                    type="text"
+                    id="interv_nom"
+                    name="nom"
+                    onChange={handleChange}
                     required
+                    className="form-control"
                   />
-                </label>
-              </div>
-              <div className="form-inter-box-files">
-                <label htmlFor="upload_carte_vitale" className="btn">
-                  <p>Une copie de votre carte vitale</p>
-                  <div>
-                    <FaCloudUploadAlt className="upload_icon" />
-                  </div>
-                  {isDocSend("CarteVitale")}
-                  <input
-                    id="upload_carte_vitale"
-                    type="file"
-                    name="CarteVitale"
-                    onChange={(e) => {
-                      handleChange(e, "file");
-                    }}
-                    className="inputfile"
-                    required
-                  />
-                </label>
-              </div>
-              <div className="form-inter-box-files">
-                <label htmlFor="upload_cv" className="btn">
-                  <p>Votre CV</p>
-                  <div>
-                    <FaCloudUploadAlt className="upload_icon" />
-                  </div>
-                  {isDocSend("CV")}
-                  <input
-                    id="upload_cv"
-                    type="file"
-                    name="CV"
-                    onChange={(e) => {
-                      handleChange(e, "file");
-                    }}
-                    className="inputfile"
-                    required
-                  />
-                </label>
-              </div>
-            </div>
+                  <label
+                    htmlFor="interv_nom"
+                    className="form-control-placeholder"
+                  >
+                    Nom
+                  </label>
+                </div>
 
-            <div className=" form-group">
-              <textarea
-                id="message"
-                name="pre_inscription_message"
-                onChange={handleChange}
-                className="form-control"
-              />
-              <label htmlFor="message" className="form-control-placeholder">
-                Votre message
-              </label>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    id="interv_prenom"
+                    name="prenom"
+                    onChange={handleChange}
+                    required
+                    className="form-control"
+                  />
+                  <label
+                    htmlFor="interv_prenom"
+                    className="form-control-placeholder"
+                  >
+                    Prénom{" "}
+                  </label>
+                </div>
+              </div>
+
+              <div className="box_form">
+                <div className="form-group">
+                  <input
+                    type="email"
+                    id="interv_email"
+                    name="email"
+                    onChange={handleChange}
+                    required
+                    className="form-control"
+                  />
+                  <label
+                    htmlFor="interv_email"
+                    className="form-control-placeholder"
+                  >
+                    Email{" "}
+                  </label>
+                </div>
+
+                <div className="form-group">
+                  <input
+                    type="text"
+                    id="interv_tel"
+                    name="telephone"
+                    onChange={handleChange}
+                    required
+                    className="form-control"
+                  />
+                  <label
+                    htmlFor="interv_tel"
+                    className="form-control-placeholder"
+                  >
+                    Téléphone
+                  </label>
+                </div>
+              </div>
+
+              <div className="box_form">
+                <div className="form-group">
+                  <input
+                    type="password"
+                    name="password"
+                    id="interv_mdp"
+                    required
+                    onChange={handleChange}
+                    autoComplete="off"
+                    className="form-control"
+                  />
+                  <label
+                    htmlFor="interv_mdp"
+                    className="form-control-placeholder"
+                  >
+                    Choisir un mot de passe
+                  </label>
+                </div>
+
+                <div className="form-group">
+                  <input
+                    type="password"
+                    name="passCheck"
+                    id="interv_mdp2"
+                    required
+                    onChange={handleChange}
+                    autoComplete="off"
+                    className="form-control"
+                  />
+                  <label
+                    htmlFor="interv_mdp2"
+                    className="form-control-placeholder"
+                  >
+                    Retapez votre mot de passe
+                  </label>
+                </div>
+              </div>
+
+              <div className="form_upload">
+                <h2>
+                  Merci de nous transmettre via ce formulaire les pièces
+                  justificatives suivantes :
+                </h2>
+                <div className="form-inter-box-files">
+                  <label htmlFor="upload_statut_ae" className="btn">
+                    <p>{`Une copie de votre statut d'autoentrepreneur`}</p>
+                    <div>
+                      <FaCloudUploadAlt className="upload_icon" />
+                    </div>
+                    {isDocSend("AutoE")}
+                    <input
+                      id="upload_statut_ae"
+                      type="file"
+                      name="AutoE"
+                      onChange={(e) => {
+                        handleChange(e, "file");
+                      }}
+                      className="inputfile"
+                      required
+                    />
+                  </label>
+                </div>
+                <div className="form-inter-box-files">
+                  <label htmlFor="upload_carte_vitale" className="btn">
+                    <p>Une copie de votre carte vitale</p>
+                    <div>
+                      <FaCloudUploadAlt className="upload_icon" />
+                    </div>
+                    {isDocSend("CarteVitale")}
+                    <input
+                      id="upload_carte_vitale"
+                      type="file"
+                      name="CarteVitale"
+                      onChange={(e) => {
+                        handleChange(e, "file");
+                      }}
+                      className="inputfile"
+                      required
+                    />
+                  </label>
+                </div>
+                <div className="form-inter-box-files">
+                  <label htmlFor="upload_cv" className="btn">
+                    <p>Votre CV</p>
+                    <div>
+                      <FaCloudUploadAlt className="upload_icon" />
+                    </div>
+                    {isDocSend("CV")}
+                    <input
+                      id="upload_cv"
+                      type="file"
+                      name="CV"
+                      onChange={(e) => {
+                        handleChange(e, "file");
+                      }}
+                      className="inputfile"
+                      required
+                    />
+                  </label>
+                </div>
+              </div>
+
+              <div className=" form-group">
+                <textarea
+                  id="message"
+                  name="pre_inscription_message"
+                  onChange={handleChange}
+                  className="form-control"
+                  required
+                />
+                <label htmlFor="message" className="form-control-placeholder">
+                  Votre message
+                </label>
+              </div>
             </div>
             <div className="submit_button">
               <button
